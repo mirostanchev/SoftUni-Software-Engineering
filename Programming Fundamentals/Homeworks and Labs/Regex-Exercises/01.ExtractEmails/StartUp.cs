eda1fa7791342afa0682e5c71540ed26290c9d0b
mirostanchev <mirostanchev@abv.bg>
2017-11-08 15:35:13 +0200

    Add exercises

---
+++ Programming Fundamentals/Homeworks and Labs/Regex-Exercises/01.ExtractEmails/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.ExtractEmails
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var textItems = Console.ReadLine();
+
+            var pattern = @"\b(?<!\S)[a-z][a-z0-9\.\-_]+[a-z0-9]*@[a-z][a-z\-]+\.[a-z][a-z\.]+[a-z]?\b";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(textItems);
+
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Homeworks and Labs/Regex-Exercises/02.ExtractSentencesByKeyword/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _02.ExtractSentencesByKeyword
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputPattern = Console.ReadLine().Trim();
+            var text = Console.ReadLine().Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var pattern = "\\b" + inputPattern + "\\b";
+            var regex = new Regex(pattern);
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (regex.IsMatch(text[i]))
+                {
+                    Console.WriteLine(text[i].Trim());
+                }
+            }
+        }
+    }
+}
