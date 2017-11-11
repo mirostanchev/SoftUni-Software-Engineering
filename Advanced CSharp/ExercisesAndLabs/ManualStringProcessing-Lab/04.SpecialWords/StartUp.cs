6a22f56f9fae4171ea6413f274dd4ed3bf9d363c
mirostanchev <mirostanchev@abv.bg>
2017-11-11 17:33:38 +0200

    add exercises

---
+++ Advanced CSharp/ExercisesAndLabs/ManualStringProcessing-Lab/04.SpecialWords/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace _04.SpecialWords
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public const string Separators = "()[]<>,-!? ‘’";
+
+        public static void Main()
+        {
+            var specialWords = Console.ReadLine()
+                .Split(Separators.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var wordsInText = Console.ReadLine()
+                .Split(Separators.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var sb = new StringBuilder();
+
+            foreach (var specialWord in specialWords)
+            {
+                var specialWordCount = 0;
+
+                foreach (var word in wordsInText)
+                {
+                    if (specialWord.Equals(word)) specialWordCount++;
+                }
+
+                sb.AppendLine($"{specialWord} - {specialWordCount}");
+            }
+
+            Console.Write(sb);
+        }
+    }
+}
---
+++ Advanced CSharp/ExercisesAndLabs/ManualStringProcessing-Lab/05.ConcatenateStrings/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _05.ConcatenateStrings
+{
+    using System;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var wordsCount = int.Parse(Console.ReadLine());
+            var result = new StringBuilder();
+
+            for (int index = 0; index < wordsCount; index++)
+            {
+                result.Append($"{Console.ReadLine()} ");
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
