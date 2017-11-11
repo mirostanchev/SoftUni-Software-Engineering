d7dd2f6aafe33fda0263d9e6daabb45833b7ef47
mirostanchev <mirostanchev@abv.bg>
2017-11-11 15:55:55 +0200

    add exercises

---
+++ Advanced CSharp/ExercisesAndLabs/SetsAndDictionaries-Lab/02.SoftUniParty/StartUp.cs
@@ -0,0 +1,57 @@
+﻿namespace _02.SoftUniParty
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var vipGuests = new SortedSet<string>();
+            var regularGuests = new SortedSet<string>();
+            var input = Console.ReadLine();
+
+            while (!input.Equals("PARTY"))
+            {
+                if (char.IsDigit(input[0]))
+                {
+                    vipGuests.Add(input);
+                }
+                else
+                {
+                    regularGuests.Add(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (!input.Equals("END"))
+            {
+                if (char.IsDigit(input[0]) && vipGuests.Contains(input))
+                {
+                    vipGuests.Remove(input);
+                }
+                else if (!char.IsDigit(input[0]) && regularGuests.Contains(input))
+                {
+                    regularGuests.Remove(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(regularGuests.Count + vipGuests.Count);
+
+            foreach (var vipGuest in vipGuests)
+            {
+                Console.WriteLine(vipGuest);
+            }
+
+            foreach (var regularGuest in regularGuests)
+            {
+                Console.WriteLine(regularGuest);
+            }
+        }
+    }
+}
