5b5cb3ea75c264c0151b08adbc7c568af82cc6fe
mirostanchev <mirostanchev@abv.bg>
2017-11-20 12:49:09 +0200

    add exercises

---
+++ CSharp OOP-Advanced/ExercisesAndLabs/Reflection-Lab/01-04.Stealer/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _01_04.Stealer
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var spy = new Spy();
+            var result = spy.CollectGettersAndSetters("_01_04.Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
